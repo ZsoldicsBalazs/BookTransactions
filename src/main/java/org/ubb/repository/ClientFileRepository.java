880e081a75ac97604abb7a4208e538a75519b7b0
Örs Csiszer <csiszerors@gmail.com>
2024-10-14 18:02:10 +0300

    client file repository is ready with two find and save methods

---
+++ src/main/java/org/ubb/repository/ClientFileRepository.java
@@ -0,0 +1,87 @@
+package org.ubb.repository;
+
+import org.ubb.domain.Client;
+import org.ubb.domain.validators.Validator;
+import org.ubb.domain.validators.ValidatorException;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.StringTemplate.STR;
+
+public class ClientFileRepository extends BookStoreRepositoryImpl<Integer, Client> {
+
+    private String fileName;
+    public ClientFileRepository(Validator<Client> validator, String filePath) {
+        super(validator);
+        this.fileName = filePath;
+        readFile();
+    }
+
+
+    @Override
+    public Optional<Client> save(Client client) {
+        if (client == null) {
+            throw new RuntimeException("The given client is empty");
+        }
+        saveToFile(client);
+        return super.save(client);
+    }
+
+//    Optional<Client> delete(Integer id) {
+//
+//    }
+//
+//    Optional<Client> update(Client entity) throws ValidatorException {
+//
+//    }
+
+
+
+
+
+
+    private void readFile() {
+        try {
+            Path path = Path.of(fileName);
+            Files.lines(path)
+                    .forEach(line -> {
+                        List<String> lineData = List.of(line.split(";"));
+                        int id = Integer.parseInt(lineData.get(0));
+                        String firstName = lineData.get(1);
+                        String lastName = lineData.get(2);
+                        int age = Integer.parseInt(lineData.get(3));
+                        String address = lineData.get(4);
+                        String email = lineData.get(5);
+
+                        Client client = new Client(id, firstName, lastName, age, address, email);
+                        super.save(client);
+                    });
+        } catch (IOException | ValidatorException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void saveToFile (Client client) {
+        Path path = Path.of(fileName);
+
+        try {
+            BufferedWriter bufferedWriter = Files.newBufferedWriter(path, StandardOpenOption.APPEND);
+            String clientString =
+                    STR."\{client.getId()};\{client.getFirstName()};\{client.getLastName()};\{client.getAge()};\{client.getAddress()};\{client.getEmail()}";
+            bufferedWriter.write(clientString);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+
+        }
+    }
+
+
+}
