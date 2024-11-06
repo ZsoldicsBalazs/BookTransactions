e5db69b2560c30c2de6069851f2ed86bd574030e
ZsoldicsBalazs <119108270+ZsoldicsBalazs@users.noreply.github.com>
2024-11-06 16:20:15 +0200

    connection pool and SaveFunction in PG repo

--- src/main/java/org/ubb/repository/PostgresRepositoryImpl.java
+++ src/main/java/org/ubb/repository/PostgresRepositoryImpl.java
@@ -1,11 +1,25 @@
 package org.ubb.repository;
 
 import org.ubb.domain.BaseEntity;
+import org.ubb.domain.Book;
+import org.ubb.domain.Client;
+import org.ubb.domain.Transaction;
+import org.ubb.domain.validators.RepositoryException;
+import org.ubb.domain.validators.Validator;
 import org.ubb.domain.validators.ValidatorException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class PostgresRepositoryImpl<ID, Entity extends BaseEntity<ID>> implements Repository<ID, Entity>{
+    Validator<Entity> validator;
+
+    public PostgresRepositoryImpl(Validator<Entity> validator) {
+        this.validator = validator;
+    }
+
     /**
      * Find the entity with the given {@code id}.
      *
@@ -38,7 +52,49 @@ public class PostgresRepositoryImpl<ID, Entity extends BaseEntity<ID>> implement
      */
     @Override
     public Optional<Entity> save(Entity entity) throws ValidatorException {
-        //Balazs
+
+        try(Connection saveConnection = ConnectionPool.getConnection()) {
+           validator.validate(entity);
+
+           switch (entity) {
+               case Client c -> {
+                   PreparedStatement statement = saveConnection.prepareStatement("INSERT INTO client VALUES (?,?,?,?,?,?)");
+                   statement.setInt(1,  c.getId());
+                   statement.setString(2, c.getFirstName());
+                   statement.setString(3,c.getLastName());
+                   statement.setInt(4,c.getAge());
+                   statement.setString(5,c.getAddress());
+                   statement.setString(6,c.getEmail());
+                   statement.executeUpdate();
+               }
+               case Book b -> {
+                   PreparedStatement statement = saveConnection.prepareStatement("INSERT INTO book VALUES (?,?,?,?,?,?)");
+                   statement.setInt(1,b.getId());
+                   statement.setString(2,b.getTitle());
+                   statement.setString(3,b.getAuthor());
+                   statement.setString(4,b.getPublisher());
+                   statement.setInt(5,b.getYear());
+                   statement.setDouble(6,b.getPrice());
+                   statement.executeUpdate();
+
+               }
+               case Transaction t ->{
+                   PreparedStatement statement = saveConnection.prepareStatement("INSERT INTO transaction VALUES (?,?,?,?,?,?)");
+                   statement.setInt(1,t.getId());
+                   statement.setInt(2,t.getSoldBooksIds());
+                   statement.setInt(3,t.getClientId());
+                   statement.setDouble(4,t.getTotalAmount());
+                   statement.executeUpdate();
+               }
+
+
+               default -> throw new RuntimeException("Unknown entity type" + entity);
+           }
+        }
+        catch (SQLException e) {
+            throw new RepositoryException(e.getMessage(),e);
+        }
+
         return Optional.empty();
     }
 
